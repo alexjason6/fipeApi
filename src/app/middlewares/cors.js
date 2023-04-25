f3a4bc9855bf949ea8049c66ddf77238781dd685
Alex Jason <39537192+alexjason6@users.noreply.github.com>
2023-04-25 14:21:56 -0300

    Add domains to Access Control Allow Origin

--- src/app/middlewares/cors.js
+++ src/app/middlewares/cors.js
@@ -1,5 +1,5 @@
 module.exports = (request, response, next) => {
-  response.setHeader('Access-Control-Allow-Origin', 'https://www.acuidar.com.br');
+  response.setHeader('Access-Control-Allow-Origin', 'http://acuidar.com.br/*, https://*.acuidar.com.br');
   response.setHeader('Access-Control-Allow-Methods', 'GET, POST');
   response.setHeader('Access-Control-Allow-Headers', '*');
   next();
