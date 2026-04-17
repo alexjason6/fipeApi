260d6cd8896dbbbc7d1d6c487c468445aab45601
Alex Jason <alexjason@Alexs-MacBook-Pro.local>
2026-04-17 15:58:53 -0300

    includes - logs

--- src/app/repositories/FipeRepository.js
+++ src/app/repositories/FipeRepository.js
@@ -2,12 +2,16 @@ const url = 'https://veiculos.fipe.org.br/api/veiculos';
 
 class FipeRepository {
   async getTabela() {
+    console.log('getTabela', `${url}/ConsultarTabelaDeReferencia`);
     const response = await fetch(`${url}/ConsultarTabelaDeReferencia`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       }
-    }).then((res) => res.json());
+    }).then((res) => {res.json();
+      console.log('response', response);
+      console.log('res', res.json());
+    });
 
     return response;
   }
