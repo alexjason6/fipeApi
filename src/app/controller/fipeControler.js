f6a064b310b1b76ad777ab1de459345cdd88604c
Alex Jason <alexjason@Alexs-MacBook-Pro.local>
2026-04-22 19:25:06 -0300

    CREATE - Parallellum API

--- src/app/controller/fipeControler.js
+++ src/app/controller/fipeControler.js
@@ -2,6 +2,18 @@ const { useFipeOficial } = require('../../config/keys');
 const FipeRepository = require('../repositories/FipeRepository');
 const ParallelumRepository = require('../repositories/ParallelumRepository');
 
+const vehicleTypeMap = { 1: 'cars', 2: 'motorcycles', 3: 'trucks' };
+
+function toParallelumParams(data) {
+  return {
+    vehicleType: vehicleTypeMap[data.codigoTipoVeiculo] || data.vehicleType || 'cars',
+    reference:   String(data.codigoTabelaReferencia || data.reference || ''),
+    brandId:     data.codigoMarca     || data.brandId,
+    modelId:     data.codigoModelo    || data.modelId,
+    yearId:      data.ano             || data.yearId,
+  };
+}
+
 class FipeController {
   async index(request, response) {
     const tabela = useFipeOficial
@@ -14,39 +26,44 @@ class FipeController {
   async showBrands(request, response) {
     const data = request.body;
 
+    const p = toParallelumParams(data);
+
     const brands = useFipeOficial
       ? await FipeRepository.list(data, 'ConsultarMarcas')
-      : await ParallelumRepository.getBrands(data.vehicleType, data.reference);
+      : await ParallelumRepository.getBrands(p.vehicleType, p.reference);
 
     response.json(brands);
   }
 
   async showModels(request, response) {
     const data = request.body;
+    const p = toParallelumParams(data);
 
     const models = useFipeOficial
       ? await FipeRepository.list(data, 'ConsultarModelos')
-      : await ParallelumRepository.getModels(data.vehicleType, data.brandId, data.reference);
+      : await ParallelumRepository.getModels(p.vehicleType, p.brandId, p.reference);
 
     response.json(models);
   }
 
   async showYears(request, response) {
     const data = request.body;
+    const p = toParallelumParams(data);
 
     const years = useFipeOficial
       ? await FipeRepository.list(data, 'ConsultarAnoModelo')
-      : await ParallelumRepository.getYears(data.vehicleType, data.brandId, data.modelId, data.reference);
+      : await ParallelumRepository.getYears(p.vehicleType, p.brandId, p.modelId, p.reference);
 
     response.json(years);
   }
 
   async result(request, response) {
     const data = request.body;
+    const p = toParallelumParams(data);
 
     const result = useFipeOficial
       ? await FipeRepository.list(data, 'ConsultarValorComTodosParametros')
-      : await ParallelumRepository.getValue(data.vehicleType, data.brandId, data.modelId, data.yearId, data.reference);
+      : await ParallelumRepository.getValue(p.vehicleType, p.brandId, p.modelId, p.yearId, p.reference);
 
     response.json(result);
   }
